0962d6bf379f0f1b3c8998c8ac6f34095c814abd
rallysport206 <phan.tony125@gmail.com>
2020-06-18 18:44:49 -0700

    adding movie card to new file

--- app/searchMovies.js
+++ app/searchMovies.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import MovieCard from 'movieCard.js';
 
 export default function SearchMovies(){
 
@@ -7,10 +8,10 @@ export default function SearchMovies(){
 
     const [movies, setMovies] = useState([]);
 
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=34502a8777f1cfa72eccf33e8904ac5a&language=en=US&query=${query}&page=1&include_adult=false`;
+
     const searchMovies = async (e) =>{ 
         e.preventDefault();
-
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=34502a8777f1cfa72eccf33e8904ac5a&language=en=US&query=${query}&page=1&include_adult=false`;
 
         try {
             const res = await fetch(url);
@@ -32,18 +33,8 @@ export default function SearchMovies(){
                 <button className="button" type="submit">Search</button>
             </form>
             <div className="card-list">
-                {movies.filter(movie => movie.poster_path).map(movie => (
-                    <div className="card" key={movie.id}>
-                        <img className="card__image" src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-                        alt={movie.title + ' poster'}
-                        />
-                        <div className="card__content">
-                        <h1 className="card__title">{movie.title}</h1>
-                        <p><small>RELEASE DATE: {movie.release_date}</small></p>
-                        <p><small>RATING: {movie.vote_average}</small></p>
-                        <p className="card__desc">OVERVIEW: {movie.overview} </p>
-                        </div>
-                    </div>
+                {movies.filter(movie => movie.poster_path).map(movie => ( 
+                    <MovieCard movie={movie} />
                 ))}
             </div>
         </>
