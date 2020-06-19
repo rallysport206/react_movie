ab0665f4893a0cab10ae3433e76bfc1df006d368
rallysport206 <phan.tony125@gmail.com>
2020-06-18 19:27:50 -0700

    small edit push

--- app/searchMovies.js
+++ app/searchMovies.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import MovieCard from './movieCard';
 
+
 export default function SearchMovies(){
 
     //states - input query, movies
@@ -8,7 +9,7 @@ export default function SearchMovies(){
 
     const [movies, setMovies] = useState([]);
 
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=34502a8777f1cfa72eccf33e8904ac5a&language=en=US&query=${query}&page=1&include_adult=false`;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=en=US&query=${query}&page=1&include_adult=false`;
 
     const searchMovies = async (e) =>{ 
         e.preventDefault();
@@ -34,7 +35,7 @@ export default function SearchMovies(){
             </form>
             <div className="card-list">
                 {movies.filter(movie => movie.poster_path).map(movie => ( 
-                    <MovieCard movie={movie} key={movie,id} />
+                    <MovieCard movie={movie} key={movie.id}/>
                 ))}
             </div>
         </>
